f19d9d9595aae92f30c569fdc12b8ca519aafbed
RonaldsonBellande <ronaldsonbellande@gmail.com>
2023-10-02 18:38:34 -0400

    extended

--- toggle_ros_versions.sh
+++ toggle_ros_versions.sh
@@ -13,6 +13,19 @@ toggle_ros_versions() {
         sed -i 's|^source /opt/ros/foxy/setup.bash|# source /opt/ros/foxy/setup.bash|' ~/.bashrc
         sed -i 's|^# source /opt/ros/noetic/setup.bash|source /opt/ros/noetic/setup.bash|' ~/.bashrc
     fi
+
+    # Check if ROS 1 (Noetic) is uncommented
+    if grep -q "# \export ROS_VERSION=2" ~/.bashrc; then
+        # Comment out ROS 1 (Noetic) and uncomment ROS 2 (Foxy)
+        sed -i 's|^export ROS_VERSION=1|# export ROS_VERSION=1|' ~/.bashrc
+        sed -i 's|^# export ROS_VERSION=2|export ROS_VERSION=2|' ~/.bashrc
+
+    else
+        # Uncomment ROS 2 (Foxy) and comment ROS 1 (Noetic)
+        sed -i 's|^export ROS_VERSION=2|# export ROS_VERSION=2|' ~/.bashrc
+        sed -i 's|^# export ROS_VERSION=1|export ROS_VERSION=1|' ~/.bashrc
+    fi
+
 }
 
 # Call the function to toggle between ROS versions
